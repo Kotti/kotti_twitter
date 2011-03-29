def32dfc420d24811a16590398abc541fa6389eb
Daniel Nouri <daniel.nouri@gmail.com>
2011-03-29 15:25:44 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='kotti_twitter',
-      version='0.2',
+      version='0.3',
       description="Add twitter widgets to your Kotti site",
       long_description=README,
       classifiers=[
