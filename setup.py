813961d182f064da1fb963578392daf89b600cd2
Daniel Nouri <daniel.nouri@gmail.com>
2011-11-13 22:53:01 +0100

    Require Kotti>=0.2.4 which includes 'pyramid_tm'.

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
 
 tests_require = [
     'pytest-cov',
@@ -29,5 +32,5 @@ setup(name='kotti_twitter',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=['Kotti'] + tests_require,
+      install_requires=['Kotti>=0.2.4'] + tests_require,
       )
